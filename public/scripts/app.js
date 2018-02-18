7d22ef9756bb1064c290b55dc11c8349fe48350f
kellybarber <kelly.james.barber@gmail.com>
2018-02-18 09:50:47 -0500

    created -1 and reset buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -43,6 +43,43 @@ var template = React.createElement(
   )
 );
 
+var count = 0;
+var addOne = function addOne() {
+  console.log('addOne');
+};
+var minusOne = function minusOne() {
+  console.log('minusOne');
+};
+var reset = function reset() {
+  console.log('reset');
+};
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Count: ',
+    count
+  ),
+  React.createElement(
+    'button',
+    { onClick: addOne },
+    '+1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: minusOne },
+    '-1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: reset },
+    'Reset'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -20,6 +20,27 @@ const template = (
 )
 
 
+let count = 0
+const addOne = () => {
+  console.log('addOne')
+}
+const minusOne = () => {
+  console.log('minusOne')
+}
+const reset = () => {
+  console.log('reset');
+}
+
+const templateTwo = (
+  <div>
+    <h1>Count: {count}</h1>
+    <button onClick={addOne}>+1</button>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={reset}>Reset</button>
+  </div>
+)
+
+
 const appRoot  = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
