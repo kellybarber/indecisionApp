fec8ecc5b00791fde1a14521e901472f863d2831
kellybarber <kelly.james.barber@gmail.com>
2018-02-18 18:14:06 -0500

    built visibility toggle mini-app

---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,54 @@
+// My Solution
+// const app = {
+//   title: 'Visibility Toggle',
+//   text: ''
+// }
+//
+// const showText = () => {
+//   !app.text ? app.text = 'Wow, text!' : app.text = ''
+//   render()
+// }
+//
+// const render = () => {
+//   const visible = (
+//     <div>
+//       <h1>{app.title}</h1>
+//       <button onClick={showText}>{!app.text ? "Show Invisible Text" : "Hide Invisible Text"}</button>
+//       <p>{app.text}</p>
+//     </div>
+//   )
+//   ReactDOM.render(visible, appRoot)
+// }
+//
+// const appRoot  = document.getElementById('app')
+// render()
+
+
+
+// Tutorial Solution
+let visibility = false;
+
+const toggleVisibility = () => {
+  visibility = !visibility;
+  render();
+};
+
+const render = () => {
+  const jsx = (
+    <div>
+      <h1>Visibility Toggle</h1>
+      <button onClick={toggleVisibility}>
+        {visibility ? 'Hide details' : 'Show details'}
+      </button>
+      {visibility && (
+        <div>
+          <p>Hey. These are some details you can now see!</p>
+        </div>
+      )}
+    </div>
+  );
+
+  ReactDOM.render(jsx, document.getElementById('app'));
+};
+
+render();
