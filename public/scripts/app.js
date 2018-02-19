f820d8c09c8d8c956908588963afbe1f9d4d5386
kellybarber <kelly.james.barber@gmail.com>
2018-02-19 18:42:51 -0500

    refactored visibility toggle project

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 // My Solution
 // const app = {
 //   title: 'Visibility Toggle',
@@ -27,39 +35,85 @@
 
 
 // Tutorial Solution
-var visibility = false;
+// let visibility = false;
+//
+// const toggleVisibility = () => {
+//   visibility = !visibility;
+//   render();
+// };
+//
+// const render = () => {
+//   const jsx = (
+//     <div>
+//       <h1>Visibility Toggle</h1>
+//       <button onClick={toggleVisibility}>
+//         {visibility ? 'Hide details' : 'Show details'}
+//       </button>
+//       {visibility && (
+//         <div>
+//           <p>Hey. These are some details you can now see!</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+//
+//   ReactDOM.render(jsx, document.getElementById('app'));
+// };
+//
+// render();
 
-var toggleVisibility = function toggleVisibility() {
-  visibility = !visibility;
-  render();
-};
 
-var render = function render() {
-  var jsx = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      'Visibility Toggle'
-    ),
-    React.createElement(
-      'button',
-      { onClick: toggleVisibility },
-      visibility ? 'Hide details' : 'Show details'
-    ),
-    visibility && React.createElement(
-      'div',
-      null,
-      React.createElement(
-        'p',
+// Component Refactor
+var Visible = function (_React$Component) {
+  _inherits(Visible, _React$Component);
+
+  function Visible(props) {
+    _classCallCheck(this, Visible);
+
+    var _this = _possibleConstructorReturn(this, (Visible.__proto__ || Object.getPrototypeOf(Visible)).call(this, props));
+
+    _this.state = {
+      visibility: false
+    };
+
+    _this.toggleVisibility = _this.toggleVisibility.bind(_this);
+    return _this;
+  }
+
+  _createClass(Visible, [{
+    key: 'toggleVisibility',
+    value: function toggleVisibility() {
+
+      this.setState(function (prevState) {
+        return { visibility: !prevState.visibility };
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
         null,
-        'Hey. These are some details you can now see!'
-      )
-    )
-  );
+        React.createElement(
+          'h1',
+          null,
+          'Visibility Toggle'
+        ),
+        React.createElement(
+          'button',
+          { onClick: this.toggleVisibility },
+          this.state.visibility ? 'Hide Details' : 'Show Details'
+        ),
+        this.state.visibility && React.createElement(
+          'p',
+          null,
+          'These are details!'
+        )
+      );
+    }
+  }]);
 
-  ReactDOM.render(jsx, document.getElementById('app'));
-};
+  return Visible;
+}(React.Component);
 
-render();
+ReactDOM.render(React.createElement(Visible, null), document.getElementById('app'));
--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -24,32 +24,61 @@
 // render()
 
 
-
-
 // Tutorial Solution
-let visibility = false;
+// let visibility = false;
+//
+// const toggleVisibility = () => {
+//   visibility = !visibility;
+//   render();
+// };
+//
+// const render = () => {
+//   const jsx = (
+//     <div>
+//       <h1>Visibility Toggle</h1>
+//       <button onClick={toggleVisibility}>
+//         {visibility ? 'Hide details' : 'Show details'}
+//       </button>
+//       {visibility && (
+//         <div>
+//           <p>Hey. These are some details you can now see!</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+//
+//   ReactDOM.render(jsx, document.getElementById('app'));
+// };
+//
+// render();
 
-const toggleVisibility = () => {
-  visibility = !visibility;
-  render();
-};
 
-const render = () => {
-  const jsx = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={toggleVisibility}>
-        {visibility ? 'Hide details' : 'Show details'}
-      </button>
-      {visibility && (
-        <div>
-          <p>Hey. These are some details you can now see!</p>
-        </div>
-      )}
-    </div>
-  );
+// Component Refactor
+class Visible extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visibility: false
+    }
 
-  ReactDOM.render(jsx, document.getElementById('app'));
-};
+    this.toggleVisibility = this.toggleVisibility.bind(this)
+  }
+  toggleVisibility() {
 
-render();
+    this.setState((prevState) => { return { visibility: !prevState.visibility } })
+
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.toggleVisibility}>
+          {this.state.visibility ? 'Hide Details' : 'Show Details'}
+        </button>
+        {this.state.visibility && <p>These are details!</p>}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Visible/>, document.getElementById('app'))
